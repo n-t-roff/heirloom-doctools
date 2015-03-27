55cbd296f4ac3ebba728a71ae4969a84ca2e3056
n-t-roff <troff@arcor.de>
2015-03-27 17:18:56 +0100

    man, mdoc: Special characters for troff updated

--- troff/troff.d/tmac.d/Makefile.mk
+++ troff/troff.d/tmac.d/Makefile.mk
@@ -2,14 +2,16 @@ MACS = acm.me bib chars.me deltext.me e eqn.me \
 	float.me footnote.me index.me local.me m mmn mmt ms.acc \
 	ms.cov ms.eqn ms.ref ms.tbl ms.ths ms.toc null.me refer.me \
 	s sh.me tbl.me thesis.me v vgrind \
-	an andoc doc doc-common doc-ditroff doc-nroff doc-syms \
+	tmac.gchar andoc doc doc-common doc-nroff doc-syms \
 	pictures color pm srefs ptx safe g padj taa naa
+IMACS =	an doc-ditroff
+OMACS = $(IMACS:%=%.out)
 
 .SUFFIXES: .in
 .in:
 	sed 's:@MACDIR@:$(MACDIR):; s:@LIBDIR@:$(LIBDIR):' $< >$@
 
-all: $(MACS)
+all: $(MACS) $(OMACS)
 
 install: all
 	test -d $(ROOT)$(MACDIR) || mkdir -p $(ROOT)$(MACDIR)
@@ -17,11 +19,17 @@ install: all
 	do \
 		$(INSTALL) -c -m 644 $$i $(ROOT)$(MACDIR)/$$i || exit; \
 	done
+	for i in $(IMACS); do \
+		$(INSTALL) -c -m 644 $${i}.out $(ROOT)$(MACDIR)/$$i || exit; \
+	done
 	test -d $(ROOT)$(MANDIR)/man7 || mkdir -p $(ROOT)$(MANDIR)/man7
 	$(INSTALL) -c -m 644 mcolor.7 $(ROOT)$(MANDIR)/man7/mcolor.7
 	$(INSTALL) -c -m 644 mpictures.7 $(ROOT)$(MANDIR)/man7/mpictures.7
 
 clean:
-	rm -f andoc bib doc e g m s pm
+	rm -f andoc bib doc e g m s pm $(OMACS)
 
 mrproper: clean
+
+$(OMACS): tmac.gchar
+	sed 's:@MACDIR@:$(MACDIR):; s:@LIBDIR@:$(LIBDIR):' $(@:%.out=%) >$@
--- version.mk
+++ version.mk
@@ -1,2 +1,2 @@
-RELEASE = Git snapshot 150326
+RELEASE = Git snapshot 150327
 #RELEASE = release 141217
