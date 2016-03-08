n-t-roff/heirloom-doctools

commit 89b1284c1de8071f3d7bb3a308cb080b8e2f311b

Warnings removed

refer/hunt7.c
@@ -44,7 +44,10 @@ findline(char *in, char **out, int outlen, long indexdate)
 	char *p, **ftp;
 	static FILE *fa = NULL;
 	long lp, llen;
-	int len, k, nofil;
+# ifdef D1
+	int len;
+# endif
+	int k, nofil;
 
 # if D1
 	fprintf(stderr, "findline: %s\n", in);
@@ -127,7 +130,9 @@ findline(char *in, char **out, int outlen, long indexdate)
                 if (*out == NULL) {
                 	return(0);
                 }
+# ifdef D1
 		len = fread(*out, 1, llen, fa);
+# endif
 		*(*out + llen) = 0;
 # ifdef D1
 		fprintf(stderr, "length as read is %d\n",len);

version.mk
@@ -1,2 +1,2 @@
-RELEASE = Git snapshot 160210
+RELEASE = Git snapshot 160211
 #RELEASE = release 150406

refer/glue4.c
@@ -40,7 +40,7 @@ grepcall (char *in, char *out, char *arg)
 	char line[200], *s, argig[100], *cv[50];
 	char *inp, inb[500];
 	FILE *gf;
-	int c, oldc = 0, alph = 0, nv = 0;
+	int c, alph = 0, nv = 0;
 	int sv0, sv1;
 	n_strcpy (argig, arg, sizeof(argig)); 
 	n_strcat(argig, ".ig", sizeof(argig));
@@ -61,7 +61,6 @@ grepcall (char *in, char *out, char *arg)
 			cv[nv++] = inp;
 		if (alph > 6)
 			*inp = 0;
-		oldc=c;
 	}
 # if D1
 	fprintf(stderr, "%d args set up\n", nv);

troff/troff.d/dpost.d/dpost.c
@@ -3147,7 +3147,7 @@ printencsep(int *colp)
 static int *
 printencvector(struct afmtab *a)
 {
-    int	i, j, k, n, col = 0, s, w;
+    int	i, j, k, n, col = 0, s;
     int	*encmap = NULL;
 
     fprintf(gf, "/Encoding-@%s@0 [\n", a->Font.intname);
@@ -3157,7 +3157,6 @@ printencvector(struct afmtab *a)
      * for later squeezing of characters >= 0400.
      */
     s = 128 - 32;
-    w = 128;
     encmap = calloc(256 + nchtab + a->nchars, sizeof *encmap);
     col += fprintf(gf, "/.notdef");
     printencsep(&col);

commit 8cdeeab54d20c3be765fbcf188cb0764cc514555

refer:  Bug introduced in prev. commit fixed
Bug introduced in commit 89b1284c1de8071f3d7bb3a308cb080b8e2f311b
fixed.
(Reported by Anthony J. Bentley (@OpenBSD))

refer/hunt7.c
@@ -131,8 +131,9 @@ findline(char *in, char **out, int outlen, long indexdate)
                 	return(0);
                 }
 # ifdef D1
-		len = fread(*out, 1, llen, fa);
+		len =
 # endif
+		fread(*out, 1, llen, fa);
 		*(*out + llen) = 0;
 # ifdef D1
 		fprintf(stderr, "length as read is %d\n",len);

stuff/bst/bst.c
@@ -35,7 +35,6 @@
 #define TREE_EMPTY   3
 
 static int srch_node(struct bst *, union bst_val, struct bst_node **);
-static struct bst_node *alloc_node(void);
 
 /* Returns:
  *   0           No error
@@ -44,22 +43,23 @@ static struct bst_node *alloc_node(void);
 int
 bst_padd(struct bst *bst, union bst_val key, union bst_val data, int bal) {
 	struct bst_node *n, *c, *gc, *t;
-	switch (srch_node(bst, key, &n)) {
-	case NODE_FOUND:
+	int i;
+	if ((i = srch_node(bst, key, &n)) == NODE_FOUND) {
 		fprintf(stderr, "bst_add: Key does already exist\n");
 		return BST_EEXIST;
+	}
+	c = malloc(sizeof(struct bst_node));
+	c->left = c->right = NULL;
+	c->bf = 0;
+	switch (i) {
 	case INSERT_LEFT:
-		c = alloc_node();
 		n->left = c;
 		break;
 	case INSERT_RIGHT:
-		c = alloc_node();
 		n->right = c;
 		break;
 	default:
-		c = alloc_node();
 		bst->root = c;
-		break;
 	}
 	c->parent = n;
 	c->key = key;
@@ -379,12 +379,4 @@ srch_node(struct bst *bst, union bst_val key, struct bst_node **node) {
 end:
 	*node = n;
 	return retval;
-}
-
-static struct bst_node *
-alloc_node(void) {
-	struct bst_node *n = malloc(sizeof(struct bst_node));
-	n->left = n->right = NULL;
-	n->bf = 0;
-	return n;
 }

version.mk
@@ -1,2 +1,2 @@
-#RELEASE = Git snapshot 160216
-RELEASE = release 160217
+#RELEASE = Git snapshot 160308
+RELEASE = release 160308