d2d36b058e9bb8fcd002cebf752d0265bf0b70a1
n-t-roff <troff@arcor.de>
2016-11-05 14:24:16 +0100

    Makefile changes fix paths in soelim manpage

--- soelim/Makefile.mk
+++ soelim/Makefile.mk
@@ -5,7 +5,7 @@ FLAGS =
 .c.o:
 	$(CC) $(_CFLAGS) $(FLAGS) -c $<
 
-all: soelim
+all: soelim soelim.1
 
 soelim: $(OBJ)
 	$(CC) $(_CFLAGS) $(_LDFLAGS) $(OBJ) $(LIBS) -o soelim
@@ -16,6 +16,9 @@ install:
 	$(INSTALL) -c -m 644 soelim.1 $(ROOT)$(MANDIR)/man1/soelim.1
 
 clean:
-	rm -f $(OBJ) soelim core log *~
+	rm -f $(OBJ) soelim core log *~ soelim.1
 
 mrproper: clean
+
+soelim.1: soelim.1.in
+	sed 's"/usr/ucblib/doctools/tmac/"$(ROOT)$(MACDIR)/"' soelim.1.in > $@
